7b2bcfe0df287a958d369fd9a5af06eda77b2bfe
foreman23 <foremanb@oregonstate.edu>
2022-10-17 19:11:12 -0700

    "changed thumbnails on work page"

--- src/videos.js
+++ src/videos.js
@@ -1,47 +1,53 @@
-
+import anaThumb from './images/videoThumbnails/anaThumb.jpg'
+import depThumb from './images/videoThumbnails/depThumb.jpg'
+import drewThumb from './images/videoThumbnails/drewThumb.jpg'
+import gannonThumb from './images/videoThumbnails/gannonThumb.jpg'
+import wyattThumb from './images/videoThumbnails/wyattThumb.jpg'
+import tereThumb from './images/videoThumbnails/tereThumb.jpg'
+import smithRockThumb from './images/videoThumbnails/smithRockThumb.jpg'
 
 export const videos = [
 
     {
         id: 7,
         title: 'Fitness Spec Ad | BMPCC 6K + Sigma 18-35',
-        thumbnail: 'https://i9.ytimg.com/vi/ApdtAa03uQE/maxresdefault.jpg?time=1666057500000&sqp=CJyKuJoG&rs=AOn4CLDQQxWLJmIs7YBb9cqzzFffht7IZw',
+        thumbnail: wyattThumb,
         embedId: 'ApdtAa03uQE'
     },
     {
         id: 6,
         title: 'Basketball Spec Ad | BMPCC 6K + Sigma 18-35',
-        thumbnail: 'https://via.placeholder.com/2000x1000',
+        thumbnail: gannonThumb,
         embedId: 'nfHaRDPmMAg'
     },
     {
         id: 5,
         title: 'Running With Drew | BMPCC 6K + Panasonic S1',
-        thumbnail: 'https://via.placeholder.com/2000x1000',
+        thumbnail: drewThumb,
         embedId:'5Qi-NiTOlGs'
     },
     {
         id: 4,
         title: 'Living With It (Short Documentary) | BMPCC 6K',
-        thumbnail: 'https://via.placeholder.com/2000x1000',
+        thumbnail: depThumb,
         embedId: 'SNhgoPm1zBA'
     },
     {
         id: 3,
         title: 'Smith Rock - 16mm Film Emulation | Panasonic S1',
-        thumbnail: 'https://via.placeholder.com/2000x1000',
+        thumbnail: smithRockThumb,
         embedId: '6UvW0fpfRHs'
     },
     {
         id: 2,
         title: 'Dog Treat Spec Ad | BMPCC 6K + Sigma 18-35',
-        thumbnail: 'https://via.placeholder.com/2000x1000',
+        thumbnail: anaThumb,
         embedId: 'kDsmbDBVEvo'
     },
     {
         id: 1,
         title: 'DEAR TERE',
-        thumbnail: 'https://via.placeholder.com/2000x1000',
+        thumbnail: tereThumb,
         embedId: '3swyuy5BdRc'
     },
 ]
