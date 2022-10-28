cbee7f3efeb314d7c96723219464228a0dc34148
Justin Zhao <justinzhao1999@gmail.com>
2022-10-28 13:36:50 -0700

    finalized home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,33 +2,43 @@ import { Col, Container, Row } from 'react-bootstrap'
 import { TypeAnimation } from 'react-type-animation'
 import { useMediaQuery } from '@mui/material';
 import { slogans } from '../texts';
-import wyattThumb from '../images/videoThumbnails/wyattThumb.jpg'
-import gannonThumb from '../images/videoThumbnails/gannonThumb.jpg'
-import depThumb from '../images/videoThumbnails/depThumb.jpg'
+import { videos } from '../videos';
+
 
 export default function Home() {
   const isBigScreen = useMediaQuery('(min-width: 576px)')
   const typeAnimationStyle = 
-    isBigScreen ? { fontWeight: 500, fontSize: '4rem' } : {fontWeight: 300, fontSize: '200%' }
-  
+    isBigScreen 
+    ? { fontWeight: 500, fontSize: '4rem' } 
+    : { fontWeight: 300, fontSize: '200%' }
+  const videoStyle = isBigScreen ? { height: 600 } : { height: 400 }
+  const featuredVideos = videos.filter(video => video.featured)
+
   return (
     <Container className="home">
-        <TypeAnimation sequence={slogans} className='home-sloganText' wrapper='h1' speed={20} repeat={Infinity} cursor={false} style={{ typeAnimationStyle }}/>
-      <Row className="home-vidRow">
-        <Col>
-          <img src={wyattThumb} alt="home thumbnailplaceholder" className='home-vidThumbnail' />
-        </Col>
-      </Row>
-      <Row className="home-vidRow">
-        <Col>
-          <img src={gannonThumb} alt="home thumbnailplaceholder" className='home-vidThumbnail' />
-        </Col>
-      </Row>
-      <Row className="home-vidRow">
-        <Col>
-          <img src={depThumb} alt="home thumbnailplaceholder" className='home-vidThumbnail' />
-        </Col>
-      </Row>
+      <TypeAnimation
+        sequence={slogans}
+        className='home-sloganText'
+        wrapper='h1'
+        speed={20}
+        repeat={Infinity}
+        cursor={false}
+        style={{ typeAnimationStyle }}
+      />
+      {featuredVideos.map(video => (
+        <Row className="home-vidRow" key={video.id}>
+          <Col sm={10}>
+            <iframe
+              width="100%"
+              height={videoStyle.height}
+              src={`https://www.youtube.com/embed/${video.embedId}`}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </Col>
+        </Row>
+      ))}
     </Container>
   );
 }
