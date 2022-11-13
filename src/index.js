86e0c4db79dc646fec6addb5f3630b1c23202d87
Justin Zhao <justinzhao1999@gmail.com>
2022-11-12 19:28:33 -0800

    added google analytics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactGA from 'react-ga';
 import { HashRouter } from 'react-router-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactGA.initialize('G-RM96H420F4', { standardImplementation: true });
+
 root.render(
   <React.StrictMode>
     <HashRouter>
